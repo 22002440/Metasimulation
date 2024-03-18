35b999169c652fb9dcdd794fb42037bb01bc3cb6
Naoufal <amallah.naoufal@gmail.com>
2024-03-18 11:50:45 +0100

    minors changes

--- modules.py
+++
@@ -1,6 +0,0 @@
-def add (val1, val2):
-    """Fonction d'addition effectue l'opération de la fonction ADD(ri, rx, rz) addition de ri avec rx"""
-    res = val1 + val2
-
-def sub(val1, val2):
-    """"""
---
+++ ram_operators.py
@@ -0,0 +1,30 @@
+def add(val1:int, val2:int):
+
+    """Does the addition between val1 and val2 refers to the function ADD in RAM machines
+    -----------
+    Parameters:
+        val1 (int): int part of the addition 
+        val2 (int): int part of the addition
+    """
+
+    res = val1 + val2 
+
+def sub(val1: int, val2: int):
+
+    """Does the substraction between val1 and val2 refers to the function SUB in RAM machines
+    -----------
+    Parameters:
+        val1 (int): int part of the substraction
+        val2 (int): int part of the substraction 
+    """
+
+    res = val1 - val2
+
+def div(val1:int, val2:int):
+    """
+    Does the division of val1 by val2, refers to the function Div in RAM machines
+    -----------
+    Parameters:
+        val1(int): int that is divided by val2
+        val2(int): int that divides val1
+    """
