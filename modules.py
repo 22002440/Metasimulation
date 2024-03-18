b91e939262e78aaa6798284b939c4ef4792b1fc3
Naoufal <amallah.naoufal@gmail.com>
2024-03-18 10:15:41 +0100

    ecriture du txt

---
+++ modules.py
@@ -0,0 +1,6 @@
+def add (val1, val2):
+    """Fonction d'addition effectue l'opération de la fonction ADD(ri, rx, rz) addition de ri avec rx"""
+    res = val1 + val2
+
+def sub(val1, val2):
+    """"""
